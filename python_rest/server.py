770f66731db0152f8b8588a957ca636d1ada4616
JoseBoon <jose.boon@maykinmedia.nl>
2018-10-11 16:33:29 +0200

    changed method to post

--- python_rest/server.py
+++ python_rest/server.py
@@ -5,33 +5,33 @@ from restart.resource import Resource
 api = RESTArt()
 
 
-@api.route(methods=['GET'])
+@api.route(methods=['POST'])
 class Add(Resource):
     name = 'add'
 
-    def read(self, request):
+    def create(self, request):
         x = int(request.data.get('x', 0))
         y = int(request.data.get('y', 0))
 
         return { 'result': x + y}
 
 
-@api.route(methods=['GET'])
+@api.route(methods=['POST'])
 class Mul(Resource):
     name = 'mul'
 
-    def read(self, request):
+    def create(self, request):
         x = int(request.data.get('x', 0))
         y = int(request.data.get('y', 0))
 
         return { 'result': x * y}
 
 
-@api.route(methods=['GET'])
+@api.route(methods=['POST'])
 class Pow(Resource):
     name = 'pow'
 
-    def read(self, request):
+    def create(self, request):
         x = int(request.data.get('x', 0))
         y = int(request.data.get('y', 0))
 
